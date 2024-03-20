432e4719ccab6997a58dc280a3e2573611a6553a
lizaelias <elias221624@gmail.com>
2024-03-20 15:38:13 +0600

    update env file added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express =require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
 const port = process.env.PORT||5000;
@@ -20,7 +21,8 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://coffeeMoster:jjGXQqS2HB4OWC5R@cluster0.asap9hb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.asap9hb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -50,6 +52,9 @@ async function run() {
       const user = await coffeeCollection.findOne(query);
       res.send(user);
     })
+
+    
+    
 
     app.put('/coffee/:id',async(req,res)=>{
       const id =req.params.id;
